87b2bf7d885983eaf9773a7f81c4cc5b6dec3ae9
juan cornejo <juanrapmc@yahoo.com.ph>
2016-09-30 18:13:47 +0800

    Added temporary gameobject for network components with send packet script

---
+++ vwts/Assets/GameObjects/Packets/SendPackets.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendPackets : MonoBehaviour {
+
+	public GameObject packet;
+
+	public GameObject targetObject;
+
+	public float spawnRate = 10f;
+	public float packetDestroyTime = 1f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("movePackets", .0f, 1/spawnRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void movePackets(){
+		GameObject p = (GameObject)Instantiate (packet, new Vector3 (15, 25 + 0.15f, 0), Quaternion.Euler(0,0,90));
+		iTween.MoveTo (p, iTween.Hash("x", targetObject.transform.position.x, "y", targetObject.transform.position.y, "z", targetObject.transform.position.z, "easeType", "easeInOutSine", "loopType", "loop", "delay", .1));
+		Destroy (p, packetDestroyTime);
+	}
+}
