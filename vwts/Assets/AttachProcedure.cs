33e7449506f835f4cd38d3006bb91f3d8eb1dc84
juan cornejo <juanrapmc@yahoo.com.ph>
2016-10-01 02:57:35 +0800

    Added attach procedure script

---
+++ vwts/Assets/AttachProcedure.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AttachProcedure : MonoBehaviour {
+	public PacketSender uePacketSender;
+
+	private PacketSender enbPacketSender;
+	private PacketSender mmePacketSender;
+
+	// Use this for initialization
+	void Start () {
+		try{
+			enbPacketSender = uePacketSender.GetComponent <PacketSender> ().targetObject.GetComponent<PacketSender> ();
+			mmePacketSender = uePacketSender.GetComponent <PacketSender> ().targetObject.GetComponent<PacketSender> ().targetObject.GetComponent<PacketSender> ();
+			StartCoroutine(AttachUe());
+		}
+		catch(Exception){
+			Debug.LogError ("Drag UE's PacketSender script to AttachProcedure script");
+		}
+	}
+
+
+	IEnumerator AttachUe()
+	{
+		uePacketSender.StartSendingPackets ();
+		yield return new WaitForSeconds(2);
+		enbPacketSender.StartSendingPackets ();
+		yield return new WaitForSeconds(2);
+		mmePacketSender.StartSendingPackets ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
